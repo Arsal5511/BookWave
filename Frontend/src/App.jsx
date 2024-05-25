afb51a0dd104dd529d5525a170d4c36d938c6597
Arslan <arslanabbas5511@gmail.com>
2024-05-26 00:24:08 +0500

    Navbar Completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,12 @@
-// import React from 'react'
+/* eslint-disable */
+
+import React from 'react'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   return (
     <div>
-      hello book lovers 
-      <h1 className="text-3xl font-bold text-red-300 underline">
-      Hello world!
-    </h1>
+      <Navbar />
     </div>
   )
 }
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,3 +1,6 @@
+/* eslint-disable */
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,5 +10,8 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+    
+  ],
 }
