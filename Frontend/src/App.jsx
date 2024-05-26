2874b3faa910cf2c0d4d0de96a0e93db362ad78f
Arslan <arslanabbas5511@gmail.com>
2024-05-27 00:04:51 +0500

    footer added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,11 +2,15 @@
 
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
+import Banner from './components/banner/Banner'
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Banner />
+      <Footer />
     </div>
   )
 }
