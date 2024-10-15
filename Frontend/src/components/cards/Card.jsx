79331e3fa98ea6b6f5cfdeebf420273f890d2ca4
arslan <arslanabbas5511@gmail.com>
2024-10-15 20:04:36 +0500

    minor settings of css

--- Frontend/src/components/cards/Card.jsx
+++ Frontend/src/components/cards/Card.jsx
@@ -2,33 +2,31 @@
 
 import React from "react";
 
-function Card({item}) {
-  console.log(item)
+function Card({ item }) {
+  console.log(item);
   return (
-    <> 
-       <div className="z-10">
-      <div className="card bg-base-100 w-96 shadow-xl">
-<div className="container">
-          <img
-            src={item.image}
-            alt="book"
-          />
-</div>
-        <div className="card-body">
-          <h2 className="card-title">
-            {item.name}
-            <div className="badge badge-secondary">NEW</div>
-          </h2>
-          <p>{item.disc}.</p>
-          <div className="card-actions justify-end">
-            <div className="badge badge-outline">{item.price}</div>
-            <div className={item.category == "free" ? "badge badge-success" : "badge badge-danger" }>{item.category}</div>
+    <>
+      <div className="mt-4 my-3 p-3">
+        <div className="card w-92 bg-base-100 shadow-xl hover:scale-105 duration-200 dark:bg-slate-900 dark:text-white dark:border">
+          <figure>
+            <img src={item.image} alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              {item.name}
+              <div className="badge badge-secondary">{item.category}</div>
+            </h2>
+            <p>{item.title}</p>
+            <div className="card-actions justify-between">
+              <div className="badge badge-outline">${item.price}</div>
+              <div className=" cursor-pointer px-2 py-1 rounded-full border-[2px] hover:bg-pink-500 hover:text-white duration-200">
+                Buy Now
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>   
     </>
-    
   );
 }
 
--- Frontend/src/components/freebooks/FreeBook.jsx
+++ Frontend/src/components/freebooks/FreeBook.jsx
@@ -13,8 +13,7 @@ function FreeBook() {
   const filteredData = list.filter((data) => data.category === "free");
   console.log("filtered Data  ", filteredData);
 
-
-  // This setting for the responsive slider is taken from https://react-slick.neostack.com/docs/example/responsive 
+  // This setting for the responsive slider is taken from https://react-slick.neostack.com/docs/example/responsive
   var settings = {
     dots: true,
     infinite: false,
@@ -29,50 +28,45 @@ function FreeBook() {
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          initialSlide: 2
-        }
+          initialSlide: 2,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
-
   return (
-    <>
-      <div className="mx-auto max-w-screen-2xl container md:px-20 px-4">
+    <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+      <div>
+        <h1 className="font-semibold text-xl pb-2">Free Offered Courses</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+          veritatis alias pariatur ad dolor repudiandae eligendi corporis nulla
+          non suscipit, iure neque earum?
+        </p>
+      </div>
 
-        <div>
-          <h1 className="text-xl pb-2 font-semibold">Free Offered Books</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil,
-            aliquid officiis. Eligendi aspernatur non blanditiis maiores, iusto
-            iure distinctio aliquid! Ducimus aliquam ex dicta, ipsam praesentium
-            debitis? Unde, autem eos?
-          </p>
-        </div>
-          <div className="slider-container">
-            <Slider {...settings}>
-              {filteredData.map((item) => (
-                <Card item={item} key={item.id} />
-              ))}
-            </Slider>
-          </div>
-        </div>
-
-    </>
+      <div className="slider-container">
+        <Slider {...settings}>
+        {filteredData.map((item) => (
+          <Card key={item.id} item={item} />
+        ))}  
+        </Slider>
+      </div>
+    </div>
   );
 }
 
